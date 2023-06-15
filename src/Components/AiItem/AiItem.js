cb67c6342a5779facb027abae93ec3a47f536e2e
Laura Matiukievic <laura.matiukievic@gmail.com>
2023-06-15 15:43:05 +0300

    item exists

--- src/Components/AiItem/AiItem.js
+++ src/Components/AiItem/AiItem.js
@@ -3,23 +3,30 @@ import Container from "../Container/container";
 
 function AiItem({ item }) {
   let { name, age, nationalize, gender } = item;
+
+  function itemExists() {
+    if (name && age && nationalize && gender ){
+        return true
+    } else {return false}
+  }
   return (
-    <Container>
-      <div>
-        <p>Name: {name.charAt(0).toUpperCase() + name.slice(1)}</p>
-        <p>Age: {age}</p>
-        <p>Nationality:</p>{" "}
-        <ul>
-          {" "}
-          {nationalize.map((country, index) => (
-            <li key={index}>
-              {country.country_id} {country.probability}
-            </li>
-          ))}
-        </ul>
-        <p>Gender: {gender.charAt(0).toUpperCase() + gender.slice(1)}</p>
-      </div>
-    </Container>
+    {itemExists()&& <Container>
+        <div>
+          <p>Name: {name.charAt(0).toUpperCase() + name.slice(1)}</p>
+          <p>Age: {age}</p>
+          <p>Nationality:</p>{" "}
+          <ul>
+            {" "}
+            {nationalize.map((country, index) => (
+              <li key={index}>
+                {country.country_id} {country.probability}
+              </li>
+            ))}
+          </ul>
+          <p>Gender: {gender.charAt(0).toUpperCase() + gender.slice(1)}</p>
+        </div>
+      </Container>}
+   
   );
 }
 
